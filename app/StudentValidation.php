1ff0fa32af77a502fa388e28d98c88a0d3956e0c
Hashman Lin <coosos810609@gmail.com>
2018-08-24 19:25:43 +0800

    Add student_validation table model and migration

---
+++ app/StudentValidation.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StudentValidation extends Model
+{
+    protected $table = 'student_validation';
+    protected $fillable = [
+        'verify_year', 'is_verify', 'verify_user_id', 'order_id', 'register_no',
+        'purchase_date', 'name', 'email', 'school_name', 'file_link', 'comment'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'id', 'verify_user_id');
+    }
+}

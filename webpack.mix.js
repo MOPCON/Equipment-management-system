a7270e5b7b45dfa2d3d53a70577ba88538fc263f
UnciaX <unciaq@gmail.com>
2019-03-28 23:16:20 +0800

    #29 v.5.7.28 Asset Directory Flattened

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,8 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css')
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
     .extract(['vue', 'jquery', 'axios'], 'public/js/vendor.js');
 
 if (mix.inProduction()) {

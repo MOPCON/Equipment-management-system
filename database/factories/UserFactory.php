1516c0fd7e3882247e2fca5173e93f14a17e000e
UnciaX <unciaq@gmail.com>
2019-03-31 15:38:25 +0800

    #29 Upgrade Laravel v5.7.28 -> v.5.8.8

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -14,7 +15,7 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,

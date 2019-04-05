33818c73eb39838f8faca179cdfafe48deb3787c
Puck Wang <s9801077@gmail.com>
2019-04-05 18:31:08 +0800

    Add fullscreen button and github button

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,10 @@ import store from './store';
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(fas);
+library.add(fas, fab);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 // Include All Components
--- resources/js/include/Navbar.vue
+++ resources/js/include/Navbar.vue
@@ -1,16 +1,35 @@
 <template>
     <div class="navbar navbar-dark navbar-custom">
         <a class="navbar-brand" href="#">EMS</a>
-        <div class="dropdown">
-            <a id="dropdownMenu" class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img :src="user.avatar" class="rounded-circle">
-                <span>{{ user.name }}</span>
+
+        <div class="form-inline">
+            <a class="nav-link text-white mr-2">
+                <div class="row no-gutters">
+                    <div class="col mr-3">
+                        <a class="text-white" href="https://github.com/puckwang/Equipment-management-system" target="_blank">
+                            <font-awesome-icon :icon="['fab', 'github']" size="lg"/>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a onclick="toogleFullscreen()">
+                            <font-awesome-icon icon="expand" size="lg" />
+                        </a>
+                    </div>
+                </div>
             </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu">
-                <a class="dropdown-item" href="/logout">Logout</a>
+
+            <div class="nav-item">
+                <div class="dropdown">
+                    <a id="dropdownMenu" class="dropdown-toggle mr-1" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img :src="user.avatar" class="rounded-circle mr-1">
+                        <span>{{ user.name }}</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu">
+                        <a class="dropdown-item" href="/logout">Logout</a>
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
 </template>
 
@@ -52,7 +71,7 @@
         height: $navbar-height;
         img {
             width: 35px;
-            height:35px;
+            height: 35px;
         }
     }
     

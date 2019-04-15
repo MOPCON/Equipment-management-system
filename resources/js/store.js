9a290f0a5d746513032f65f2330b219f95ded670
Puck Wang <s9801077@gmail.com>
2019-04-15 15:36:08 +0800

    更改 store 的匯入方式，讓它可以自動匯入所有 modules

--- resources/js/store.js
+++ resources/js/store.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import auth from './stores/auth'
 
 Vue.use(Vuex);
+
+// Include All Store
+const files = require.context('./stores', true, /\.js$/i);
+let modules = {};
+files.keys().forEach(key => {
+    modules[key.split('/').pop().split('.')[0]] = files(key).default;
+});
 
 const isDebug = process.env.NODE_ENV !== 'production';
 
 const store = new Vuex.Store({
-    modules: {
-        auth: auth,
-    },
+    modules: modules,
     strict: isDebug
 });
 

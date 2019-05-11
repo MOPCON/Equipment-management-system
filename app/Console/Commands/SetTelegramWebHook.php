5a942d9c54b0bc8efc07788a48bdd77182574fbd
Puck Wang <s9801077@gmail.com>
2019-05-11 17:08:57 +0800

    Update SetTelegramWebHook command

--- app/Console/Commands/SetTelegramWebHook.php
+++ app/Console/Commands/SetTelegramWebHook.php
@@ -12,7 +12,7 @@ class SetTelegramWebHook extends Command
      *
      * @var string
      */
-    protected $signature = 'ems:set-telegram-hook {--d|delete : 刪除所有 WebHook}';
+    protected $signature = 'ems:set-telegram-hook {--d|delete : 刪除所有 WebHook} {--output}';
 
     /**
      * The console command description.
@@ -36,18 +36,42 @@ class SetTelegramWebHook extends Command
      * Execute the console command.
      *
      * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function handle()
     {
         try {
-            if ($this->option('delete')) {
-            } else {
-                $hookUrl = action('TelegramHookController@handle');
+            $client = new \GuzzleHttp\Client();
+            $url = 'https://api.telegram.org/bot'
+                . config('botman.telegram.token')
+                . '/setWebhook';
+
+            $remove = $this->option('delete', null);
+
+            if (!$remove) {
+                $url .= '?url=' . action('TelegramHookController@handle');
             }
 
+            $this->info('Using ' . $url);
 
+            $this->info('Pinging Telegram...');
+
+            $response = @$client->request('GET', $url);
+            $result = json_decode($response->getBody());
+            if ($result->ok == true && $result->result == true) {
+                $this->info(
+                    $remove
+                    ? 'Your bot Telegram\'s webhook has been removed!'
+                    : 'Your bot is now set up with Telegram\'s webhook!'
+                );
+            }
+
+            if ($this->option('output')) {
+                dump($result);
+            }
         } catch (\Exception $e) {
             $this->error('發生不明錯誤');
+            $this->error($e->getMessage());
             Log::error($e->getMessage() . $e->getTraceAsString());
         }
     }

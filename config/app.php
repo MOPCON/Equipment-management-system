716db6d46544ebaf2814d3efd8de5b8581769cb5
Xavier Wang <duley666@gmail.com>
2019-05-13 20:30:28 +0800

    remove duplicated config settings

--- config/app.php
+++ config/app.php
@@ -167,7 +167,6 @@ return [
          */
         Creativeorange\Gravatar\GravatarServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
-        Jenssegers\Agent\AgentServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -193,7 +192,6 @@ return [
 
     'aliases' => [
 
-        'Agent' => Jenssegers\Agent\Facades\Agent::class,
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,

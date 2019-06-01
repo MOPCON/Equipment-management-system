9597cbe9137549c950204557af6e796602d9c764
Puck Wang <s9801077@gmail.com>
2019-06-02 01:23:03 +0800

    Add 403 page

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import errorTextFormat from './filters/errorTextFormat'
 import timeFormat from './filters/timeFormat'
 import VeeValidate from 'vee-validate'
+import axios from 'axios'
 
 Vue.filter('timeFormat', timeFormat)
 Vue.filter('errorTextFormat', errorTextFormat)
@@ -26,6 +27,16 @@ const files = require.context('./components', true, /\.vue$/i);
 files.keys().forEach(key => {
     Vue.component(key.split('/').pop().split('.')[0], files(key).default);
 });
+
+// Add global 403 error Handler
+axios.interceptors.response.use(
+    response => response,
+    (error) => {
+        if (error.response.status === 403) {
+            router.push('403')
+        }
+    }
+);
 
 const vue = new Vue({
     el: '#app',

65a8c08a57c15143532dca4c7776a0113e64fc08
Puck Wang <s9801077@gmail.com>
2019-10-18 22:36:55 +0800

    修復工人列表組別篩選錯誤的問題

--- app/Http/Controllers/TelegramHookController.php
+++ app/Http/Controllers/TelegramHookController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 class TelegramHookController extends Controller
 {
+    public function __construct()
+    {
+        require base_path('routes/botman.php');
+    }
+
     public function handle()
     {
         $botman = app('botman');

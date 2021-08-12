ce3f7173cbd779d5afee43422a62d34d6cbf694a
Eric Fang <ree0916tw@gmail.com>
2021-08-13 00:50:35 +0800

    change vue cdn

--- resources/views/form/sponsor.blade.php
+++ resources/views/form/sponsor.blade.php
@@ -592,7 +592,7 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.min.js"></script>
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
